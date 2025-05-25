9cd74b387dd478ee56015f83208749fbcf92171c
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-05-25 16:12:16 +0900

    레이아웃 구성 1

---
+++ lib/ui/prayer_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PrayerList extends StatelessWidget {
+  const PrayerList({
+    super.key,
+    list
+  });
+
+  List<PrayerList> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: 9,
+      child: ,
+    );
+  }
+}
